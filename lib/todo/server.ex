a53564ed36b9a55397cfc32af80c14c93c7ae8e0
KuKuRammus <vladkamyshanov@gmail.com>
2022-06-05 22:15:59 +0300

    Add timeout parameter to the todo server process

--- lib/todo/server.ex
+++ lib/todo/server.ex
@@ -2,6 +2,9 @@ defmodule Todo.Server do
   # Use gen_server behaviour (behaviours are similar to traits+interface to PHP?)
   use GenServer, restart: :temporary
 
+  # After how long server should shutdown due to inactivity
+  @expiry_idle_timeout :timer.seconds(10)
+
   # Interface function: start server
   def start_link(name) do
     # Start server and register it into registry
@@ -26,7 +29,11 @@ defmodule Todo.Server do
   @impl GenServer
   def init(list_name) do
     IO.puts("Starting Todo.Server - #{list_name}")
-    {:ok, {list_name, Todo.Database.get(list_name) || Todo.List.new()}} # Try to fetch from database with fallback
+    {
+      :ok,
+      {list_name, Todo.Database.get(list_name) || Todo.List.new()}, # Try to fetch from database with fallback
+      @expiry_idle_timeout
+    }
   end
 
   # Implement handle_cast from gen_server (takes: request, current_state)
@@ -37,14 +44,26 @@ defmodule Todo.Server do
     # Persist data
     Todo.Database.store(list_name, new_state)
 
-    {:noreply, {list_name, new_state}} # Casts must return {:noreply, new_state}
+    {:noreply, {list_name, new_state}, @expiry_idle_timeout} # Casts must return {:noreply, new_state} + optional timeout
   end
 
   # Implement handle_call from gen_server (takes: request, caller info(request ID, used internally), current_state)
   @impl GenServer
   def handle_call({:entries, date}, _, {list_name, todo_list}) do
     entries = Todo.List.entries(todo_list, date)
-    {:reply, entries, {list_name, todo_list}} # Calls must return {:reply, response, new_state}
+    {
+      :reply,
+      entries,
+      {list_name, todo_list},
+      @expiry_idle_timeout # Add timeout parameter
+      } # Calls must return {:reply, response, new_state}
+  end
+
+  # Handle timeout message
+  @impl GenServer
+  def handle_info(:timeout, {name, todo_list}) do
+    IO.puts("Stopping to-do server #{name}")
+    {:stop, :normal, {name, todo_list}}
   end
 
 end
